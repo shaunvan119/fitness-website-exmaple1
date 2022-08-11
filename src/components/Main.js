197e228eef90aa28861e3aead819bcfd9f4c0c12
shaunvan119 <shaunvan@live.com.au>
2022-08-11 18:11:25 +1000

    added background video and bottom to top component folders

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,13 @@ import videoBg from "../assets/videoBg.mp4"
 const Main = () => {
   return (
     <div className='main'>
+    <div className='overlay'></div>
     <video src={videoBg} autoPlay loop muted/>
+    <div className="content">
+    <h1>Thailand Health Retreat</h1>
+    <p>Launching Soon</p>
+
+    </div>
     
     </div>
   )
