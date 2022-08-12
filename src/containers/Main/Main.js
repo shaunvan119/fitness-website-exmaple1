054370f90d27e258b8c251fde8a95e6a31e38e6e
shaunvan119 <shaunvan@live.com.au>
2022-08-12 17:23:08 +1000

    css layout

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,20 +1,30 @@
 import './main.css'
 import videoBg from "../../assets/videoBg.mp4"
 import Count from '../../components/Count'
+import logo from "../../assets/logo.png"
+import CalenderDates from '../../components/CalenderDates'
+
 
 const Main = () => {
   return (
+
+    
     
     <div className='main'>
+  
     <div className='overlay'></div>
+    <button className='button-dates'>Check dates</button>
     <video src={videoBg} autoPlay loop muted/>
     <div className="content">
+    <img src={logo} alt="Logo" className="logo" />
     <h1 className='heading-main'>Thailand Health Retreat</h1>
     <p className='p-main'>Launching Soon</p>
     <Count/>
+    <CalenderDates/>
     </div>
     
     </div>
+    
   )
 }
 
